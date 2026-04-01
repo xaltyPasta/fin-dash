8bd2dd157fc4fd1b6c11d129a7f5863d0e841141
xaltyPasta <rajkumarravi9470@gmail.com>
2026-04-01 19:10:16 +0530

    types added

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,8 @@
+import "dotenv/config";
+
 export const config = {
   PORT: process.env.PORT || 3000,
   DATABASE_URL: process.env.DATABASE_URL!,
   JWT_SECRET: process.env.JWT_SECRET!,
+  NODE_ENV: process.env.NODE_ENV || "development",
 };
